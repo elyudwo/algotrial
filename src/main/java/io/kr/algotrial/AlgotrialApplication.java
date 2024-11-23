bc4063068d6918a48d1a9c563f64c900238d5138
elyudwo <dudqk9696@naver.com>
2024-11-23 15:24:02 +0900

    init project

---
+++ src/main/java/io/kr/algotrial/AlgotrialApplication.java
@@ -0,0 +1,13 @@
+package io.kr.algotrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgotrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgotrialApplication.class, args);
+    }
+
+}
