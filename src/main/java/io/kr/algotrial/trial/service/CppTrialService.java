e107682fbda8ba16479e3b3d32baae59573aa613
elyudwo <dudqk9696@naver.com>
2024-12-03 21:59:43 +0900

    [Feat]: Add time complexity calculation logic to the Request CPP code

--- src/main/java/io/kr/algotrial/trial/service/CppTrialService.java
+++ src/main/java/io/kr/algotrial/trial/service/CppTrialService.java
@@ -1,22 +1,28 @@
 package io.kr.algotrial.trial.service;
 
+import io.kr.algotrial.trial.dto.CodeReqDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class CppTrialService {
 
-    public Process runCompiledProcess(Path binaryFilePath, String input, double timeComplexity) {
+    private final TimeSpaceComplexityValidator timeSpaceComplexityValidator;
+
+    public Process runCompiledProcess(Path binaryFilePath, String input, CodeReqDto codeReqDto) {
         try {
+            log.info("run 진입 성공");
             ProcessBuilder runBuilder = new ProcessBuilder(binaryFilePath.toString());
             runBuilder.redirectErrorStream(true);
             Process process = runBuilder.start();
+            log.info("Process 시작 성공");
 
             try (OutputStream outputStream = process.getOutputStream()) {
                 log.info("Input data being sent to the process: '{}' by CPP", input);
@@ -24,23 +30,9 @@ public class CppTrialService {
                 outputStream.flush();
             }
 
-            validateTimeComplexity(process, timeComplexity);
-
             return process;
         } catch (IOException e) {
             throw new RuntimeException("Failed to run C++ compile ", e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Execution interrupted ", e);
-        }
-    }
-
-    private void validateTimeComplexity(Process process, double timeComplexity) throws InterruptedException {
-        long timeoutInMillis = (long) (timeComplexity * 1000);
-        boolean checkTimeout = process.waitFor(timeoutInMillis, TimeUnit.MILLISECONDS);
-
-        if(!checkTimeout) {
-            process.destroy();
-            throw new RuntimeException("Execution Timeout: Process took too long to complete.");
         }
     }
 
